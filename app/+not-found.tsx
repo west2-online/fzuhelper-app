1e6f3a0dc58027b5e37471450cfb804cc84c8c2d
Baoshuo <i@baoshuo.ren>
2024-10-28 19:07:50 +0800

    refactor: switch to tamagui

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,18 +1,25 @@
-import { Link, Stack } from 'expo-router';
-import { Text } from 'react-native';
+import { Stack } from 'expo-router';
+import { H1, View } from 'tamagui';
 
-import { ThemedView } from '@/components/ThemedView';
+import { LinkButton } from '@/components/Link';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView className="flex items-center justify-center p-5">
-        <Text>This screen doesn't exist.</Text>
-        <Link href="/" className="mt-4 py-4">
-          Go to home screen!
-        </Link>
-      </ThemedView>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="$6"
+        minHeight="100vh"
+      >
+        <H1>页面不存在</H1>
+
+        <LinkButton href="/" variant="outlined">
+          返回首页
+        </LinkButton>
+      </View>
     </>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
+    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+    plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
