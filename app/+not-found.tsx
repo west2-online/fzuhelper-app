762c0f986e9123b11abf1e49da80c3a72b24c8f0
Baoshuo <i@baoshuo.ren>
2024-11-03 14:40:55 +0800

    Revert "refactor: switch to tamagui (#16)" (#17)
    This reverts commit ba4ebd509a230759213709f5ad9f3728183ea508.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,25 +1,18 @@
-import { Stack } from 'expo-router';
-import { H1, View } from 'tamagui';
+import { Link, Stack } from 'expo-router';
+import { Text } from 'react-native';
 
-import { LinkButton } from '@/components/Link';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ headerShown: false }} />
-      <View
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        gap="$6"
-        minHeight="100vh"
-      >
-        <H1>页面不存在</H1>
-
-        <LinkButton href="/" variant="outlined">
-          返回首页
-        </LinkButton>
-      </View>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <ThemedView className="flex items-center justify-center p-5">
+        <Text>This screen doesn't exist.</Text>
+        <Link href="/" className="mt-4 py-4">
+          Go to home screen!
+        </Link>
+      </ThemedView>
     </>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const { withTamagui } = require('@tamagui/metro-plugin');
+const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts.push('mjs')
-
-module.exports = withTamagui(config, {
-  components: ['tamagui'],
-  config: './tamagui.config.ts',
-  outputCSS: './tamagui-web.css',
+module.exports = withNativeWind(config, {
+  input: './global.css',
+  inlineRem: false,
 });
