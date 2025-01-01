7d0cbe7f2ea43580c433826a4ea6139b98abe9bb
Baoshuo <i@baoshuo.ren>
2025-01-01 23:31:14 +0800

    feat: user login

--- modules/native-request/index.ts
+++ modules/native-request/index.ts
@@ -2,10 +2,19 @@
 // and on native platforms to NativeRequest.ts
 import NativeRequestModule from './src/NativeRequestModule';
 
+interface NativeRequestResponse {
+  status: number;
+  data: Uint8Array;
+  headers: Record<string, string>;
+}
+
 // 返回格式为 { status: number, data: Uint8Array, headers: Record<string, string> }
 // iOS 原始响应格式为 { status: Int?, data: Data?, headers: Record<string, string>, error: String? }
 // Android 原始响应格式为 { status: Int, data: ByteArray?, headers: Map<String, List<String>> }
-export async function get(url: string, headers: Record<string, string>) {
+export async function get(
+  url: string,
+  headers: Record<string, string>,
+): Promise<NativeRequestResponse> {
   const response = await NativeRequestModule.get(url, { data: headers });
   if (response.error) {
     throw new Error(response.error);
@@ -24,7 +33,7 @@ export async function post(
   url: string,
   headers: Record<string, string>,
   formData: Record<string, string>,
-) {
+): Promise<NativeRequestResponse> {
   const response = await NativeRequestModule.post(
     url,
     { data: headers },
