cd62fee06296bef5b8558c4ccdf34025b61c8f0e
Baoshuo <i@baoshuo.ren>
2025-01-14 22:05:31 +0800

    feat: add button variants (#26)

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,57 @@
 import { Pressable, Text, type PressableProps } from 'react-native';
 
+import { cva, type VariantProps } from 'class-variance-authority';
+
 import cn from '@/utils/cn';
 import safeChildren from '@/utils/safe-children';
 
-export interface ButtonProps extends PressableProps {
+const buttonVariants = cva(
+  'inline-flex items-center justify-center gap-2 rounded-md font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'bg-black py-2 px-3',
+        link: '',
+      },
+      size: {
+        default: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
+const textVariants = cva('text-white', {
+  variants: {
+    variant: {
+      default: '',
+      link: 'text-primary',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+export interface ButtonProps extends PressableProps, VariantProps<typeof buttonVariants> {
   className?: string;
   textClassName?: string;
 }
 
-const Button: React.FC<React.PropsWithChildren<ButtonProps>> = ({ className, textClassName, children, ...props }) => (
-  <Pressable className={cn('flex items-center bg-black px-4 py-2', className)} {...props}>
+const Button: React.FC<React.PropsWithChildren<ButtonProps>> = ({
+  className,
+  textClassName,
+  variant,
+  size,
+  children,
+  ...props
+}) => (
+  <Pressable className={cn(buttonVariants({ variant, size, className }))} {...props}>
     {safeChildren(children, p => (
-      <Text className={cn('text-white', textClassName)} {...p} />
+      <Text className={cn(textVariants({ variant, className: textClassName }))} {...p} />
     ))}
   </Pressable>
 );
