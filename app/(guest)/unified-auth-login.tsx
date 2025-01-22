f56f49acbaff4d119b5da373fbc71f548dec2bb1
klxiaoniu <klxiaoniu@proton.me>
2025-01-22 18:16:45 +0800

    fix: make unifiedLoginPage keyboard avoiding

--- app/(guest)/unified-auth-login.tsx
+++ app/(guest)/unified-auth-login.tsx
@@ -8,7 +8,16 @@ import YMTLogin from '@/lib/ymt-login';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Stack, router } from 'expo-router';
 import { useCallback, useRef, useState } from 'react';
-import { Alert, Linking, ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Linking,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const NAVIGATION_TITLE = '统一身份认证';
@@ -90,84 +99,86 @@ const UnifiedLoginPage: React.FC = () => {
       <Stack.Screen options={{ title: NAVIGATION_TITLE }} />
 
       <SafeAreaView className="bg-background" edges={['bottom', 'left', 'right']}>
-        <ScrollView
-          className="h-full"
-          contentContainerStyle={styles.scrollViewContent}
-          keyboardShouldPersistTaps="handled"
-        >
-          <ThemedView className="flex-1 justify-between px-6 py-3">
-            {/* 左上角标题 */}
-            <View className="ml-1 mt-14">
-              <Text className="mb-2 text-3xl font-bold">统一身份认证平台登录</Text>
-            </View>
-
-            {/* 页面内容 */}
-            <View className="items-center justify-center">
-              {/* 用户名输入框 */}
-              <Input
-                value={account}
-                onChangeText={setAccount}
-                placeholder="请输入学号"
-                className="my-4 w-full px-1 py-3"
-              />
-
-              {/* 密码输入框 */}
-              <Input
-                value={accountPassword}
-                onChangeText={setAccountPassword}
-                placeholder="请输入密码"
-                secureTextEntry
-                className="mb-12 w-full px-1 py-3"
-              />
-
-              {/* 登录按钮 */}
-              <TouchableOpacity
-                onPress={isLoggingIn ? undefined : handleLogin}
-                disabled={isLoggingIn}
-                className={`mb-6 w-full items-center justify-center rounded-4xl py-3 ${
-                  isLoggingIn ? 'bg-gray-400' : 'bg-primary'
-                }`}
-              >
-                <Text className="text-lg font-bold text-white">{isLoggingIn ? '登录中...' : '登 录'}</Text>
-              </TouchableOpacity>
-
-              <View className="w-full flex-row justify-end px-2">
-                {/* 忘记密码按钮 */}
-                <Text className="text-primary" onPress={openForgetPassword}>
-                  重置密码
-                </Text>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <ScrollView
+            className="h-full"
+            contentContainerStyle={styles.scrollViewContent}
+            keyboardShouldPersistTaps="handled"
+          >
+            <ThemedView className="flex-1 justify-between px-6 py-3">
+              {/* 左上角标题 */}
+              <View className="ml-1 mt-14">
+                <Text className="mb-2 text-3xl font-bold">统一身份认证平台登录</Text>
               </View>
 
-              {/* 公告栏 */}
-              <View className="mt-10 w-full px-1">
-                <Text className="my-2 text-lg font-bold text-muted-foreground">友情提示</Text>
-                <Text className="text-base text-muted-foreground">
-                  1. 统一身份认证平台为福州大学统一登录系统，可用于登录图书馆、一码通、智汇福大等平台。
-                </Text>
-                <Text className="text-base text-muted-foreground">2. 新生可使用身份证号作为登录账号。</Text>
-                <Text className="text-base text-muted-foreground">
-                  3. 福州大学教务处系统独立于统一身份认证平台，需使用专有密码登录。
+              {/* 页面内容 */}
+              <View className="items-center justify-center">
+                {/* 用户名输入框 */}
+                <Input
+                  value={account}
+                  onChangeText={setAccount}
+                  placeholder="请输入学号"
+                  className="my-4 w-full px-1 py-3"
+                />
+
+                {/* 密码输入框 */}
+                <Input
+                  value={accountPassword}
+                  onChangeText={setAccountPassword}
+                  placeholder="请输入密码"
+                  secureTextEntry
+                  className="mb-12 w-full px-1 py-3"
+                />
+
+                {/* 登录按钮 */}
+                <TouchableOpacity
+                  onPress={isLoggingIn ? undefined : handleLogin}
+                  disabled={isLoggingIn}
+                  className={`mb-6 w-full items-center justify-center rounded-4xl py-3 ${
+                    isLoggingIn ? 'bg-gray-400' : 'bg-primary'
+                  }`}
+                >
+                  <Text className="text-lg font-bold text-white">{isLoggingIn ? '登录中...' : '登 录'}</Text>
+                </TouchableOpacity>
+
+                <View className="w-full flex-row justify-end px-2">
+                  {/* 忘记密码按钮 */}
+                  <Text className="text-primary" onPress={openForgetPassword}>
+                    重置密码
+                  </Text>
+                </View>
+
+                {/* 公告栏 */}
+                <View className="mt-10 w-full px-1">
+                  <Text className="my-2 text-lg font-bold text-muted-foreground">友情提示</Text>
+                  <Text className="text-base text-muted-foreground">
+                    1. 统一身份认证平台为福州大学统一登录系统，可用于登录图书馆、一码通、智汇福大等平台。
+                  </Text>
+                  <Text className="text-base text-muted-foreground">2. 新生可使用身份证号作为登录账号。</Text>
+                  <Text className="text-base text-muted-foreground">
+                    3. 福州大学教务处系统独立于统一身份认证平台，需使用专有密码登录。
+                  </Text>
+                </View>
+              </View>
+
+              {/* 底部协议 */}
+              <View className="mb-6 mt-12 w-full flex-row justify-center">
+                <Checkbox checked={isAgree} onCheckedChange={setIsAgree} />
+                <Text className="text-center text-muted-foreground">
+                  {'  '}
+                  阅读并同意{' '}
+                  <Text className="text-primary" onPress={openUserAgreement}>
+                    用户协议
+                  </Text>{' '}
+                  和{' '}
+                  <Text className="text-primary" onPress={openPrivacyPolicy}>
+                    隐私政策
+                  </Text>
                 </Text>
               </View>
-            </View>
-
-            {/* 底部协议 */}
-            <View className="mb-6 mt-12 w-full flex-row justify-center">
-              <Checkbox checked={isAgree} onCheckedChange={setIsAgree} />
-              <Text className="text-center text-muted-foreground">
-                {'  '}
-                阅读并同意{' '}
-                <Text className="text-primary" onPress={openUserAgreement}>
-                  用户协议
-                </Text>{' '}
-                和{' '}
-                <Text className="text-primary" onPress={openPrivacyPolicy}>
-                  隐私政策
-                </Text>
-              </Text>
-            </View>
-          </ThemedView>
-        </ScrollView>
+            </ThemedView>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </>
   );
