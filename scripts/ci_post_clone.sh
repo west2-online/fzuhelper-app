966cfde1a49724e5ef17dce8e88157c19aa1ecac
ozline <ozlinex@outlook.com>
2025-01-26 20:41:51 +0800

    fix: xcode cloud compiling script error

--- scripts/ci_post_clone.sh
+++ scripts/ci_post_clone.sh
@@ -14,7 +14,7 @@ yarn install
 # xcode cloud sets `CI` env var to 'TRUE':
 # This causes a crash: Error: GetEnv.NoBoolean: TRUE is not a boolean.
 # This is a workaround for that issue.
-CI="true" npx expo prebuild:ios
+CI="true" npx expo prebuild --platform ios
 
 
 # 脚本来源：https://www.richinfante.com/2024/11/18/running-expo-prebuild-in-xcode-cloud
