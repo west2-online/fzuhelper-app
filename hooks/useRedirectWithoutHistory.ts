dc0b6092954933216fc5a48ac20b97b09c9f5a48
ozline <ozlinex@outlook.com>
2025-02-12 20:09:28 +0800

    fix: the action 'POP_TO_TOP' was not handled by any navigator

--- hooks/useRedirectWithoutHistory.ts
+++ hooks/useRedirectWithoutHistory.ts
@@ -7,8 +7,11 @@ export function useRedirectWithoutHistory() {
 
   const redirect = useCallback(
     (href: Href) => {
-      rootNavigation.dispatch(StackActions.popToTop());
-      router.replace(href);
+      // 检查是否可以返回到顶部
+      if (rootNavigation.canGoBack()) {
+        rootNavigation.dispatch(StackActions.popToTop());
+      }
+      router.replace(href); // 替换到目标路由
     },
     [rootNavigation],
   );
