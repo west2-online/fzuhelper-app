f74a2ab95d8e7e7deb8dee6e5098f4d9f6650d93
klxiaoniu <klxiaoniu@proton.me>
2025-02-18 14:01:55 +0800

    feat: implement loading view

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,5 +1,12 @@
-import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+import { Circle } from 'react-native-animated-spinkit';
 
-const Loading: React.FC = () => <Text>Loading...</Text>;
+const Loading: React.FC = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Circle size={72} color="#1089FF" />
+    </View>
+  );
+};
 
 export default Loading;
