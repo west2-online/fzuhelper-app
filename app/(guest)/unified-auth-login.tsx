1733d60397342d3f2a9b1f3d1334470b2fc88f63
柠檬味氨水 <33548986+weijianxian@users.noreply.github.com>
2025-02-19 20:56:16 +0800

    修正登录失败时的错误信息输出

--- app/(guest)/unified-auth-login.tsx
+++ app/(guest)/unified-auth-login.tsx
@@ -89,7 +89,8 @@ const UnifiedLoginPage: React.FC = () => {
     } catch (error: any) {
       const data = handleError(error);
       if (data) {
-        Alert.alert('请求失败', data.code + ': ' + data.message);
+        console.log('登录失败:', data);
+        Alert.alert('请求失败', data.code + ': ' + data.msg);
       }
     } finally {
       // 恢复按钮状态
