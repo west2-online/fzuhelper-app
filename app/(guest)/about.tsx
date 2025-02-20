9e746013b185da9dc8a1c276aaf458e069612cf2
klxiaoniu <klxiaoniu@proton.me>
2025-02-21 01:26:30 +0800

    fix: about page icon size

--- app/(guest)/about.tsx
+++ app/(guest)/about.tsx
@@ -53,7 +53,7 @@ export default function AboutPage() {
                   <Text className="text-text-secondary">官方网站</Text>
                 </DescriptionListTerm>
                 <DescriptionListDescription>
-                  <Icon name="chevron-forward" />
+                  <Icon name="chevron-forward" size={14} />
                 </DescriptionListDescription>
               </DescriptionListRow>
             </Pressable>
@@ -92,7 +92,7 @@ export default function AboutPage() {
               </Text>
             </View>
             <View className="flex-row">
-              <Text className="text-sm text-text-secondary">本APP由</Text>
+              <Text className="text-text-secondary text-sm">本APP由</Text>
               <Text
                 className="text-sm text-primary"
                 onPress={() => {
@@ -101,7 +101,7 @@ export default function AboutPage() {
               >
                 又拍云
               </Text>
-              <Text className="text-sm text-text-secondary">提供CDN加速/云存储服务</Text>
+              <Text className="text-text-secondary text-sm">提供CDN加速/云存储服务</Text>
             </View>
             <Pressable
               className="flex-row items-center"
@@ -109,10 +109,10 @@ export default function AboutPage() {
                 Linking.openURL('https://beian.miit.gov.cn/');
               }}
             >
-              <Text className="mr-1 text-sm text-text-secondary">ICP备案号：闽ICP备19020557号-4A</Text>
+              <Text className="text-text-secondary mr-1 text-sm">ICP备案号：闽ICP备19020557号-4A</Text>
               <Icon name="chevron-forward" size={10} />
             </Pressable>
-            <Text className="mb-6 text-sm text-text-tertiary">
+            <Text className="text-text-tertiary mb-6 text-sm">
               Copyright © 2017-{new Date().getFullYear()} west2-online. All Rights Reserved
             </Text>
           </SafeAreaView>
