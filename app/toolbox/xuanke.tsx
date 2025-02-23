b19bdba32c44e2ec95a58a81ffbba2c5b6580186
ozline <ozlinex@outlook.com>
2025-02-23 17:04:55 +0800

    feat: add tips to xuanke

--- app/toolbox/xuanke.tsx
+++ app/toolbox/xuanke.tsx
@@ -3,6 +3,7 @@ import { useLayoutEffect } from 'react';
 import { View } from 'react-native';
 
 import PageContainer from '@/components/page-container';
+import { Text } from '@/components/ui/text';
 
 import LabelEntry from '@/components/label-entry';
 import { pushToWebViewJWCH } from '@/lib/webview';
@@ -36,6 +37,18 @@ export default function AcademicPage() {
         {menuItems.map((item, index) => (
           <LabelEntry key={index} leftText={item.name} onPress={() => pushToWebViewJWCH(item.url, item.name)} />
         ))}
+      </View>
+      <View className="space-y-4">
+        <Text className="my-2 text-lg font-bold text-text-secondary">友情提示</Text>
+        <Text className="my-2 text-base text-text-secondary">
+          教务系统的选课功能可能存在一定的问题，如有问题请及时联系教务处
+        </Text>
+        <Text className="my-2 text-base text-text-secondary">
+          由于上述因素，我们不提供选课功能，仅提供跳转链接。您可以通过双指聚拢/散开来实现页面放大、缩小
+        </Text>
+        <Text className="my-2 text-base text-text-secondary">
+          选课结束后务必重新进入选课页面确认选课无误，如有必要可以截图保留
+        </Text>
       </View>
     </PageContainer>
   );
