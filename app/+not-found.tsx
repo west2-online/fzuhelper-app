b22253f4bfed84608cfe8f93c0cc6aa2e8bd6ca0
Baoshuo <i@baoshuo.ren>
2025-02-25 18:23:36 +0800

    chore: format codes

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,22 +1,15 @@
-import { Link, useNavigation } from 'expo-router';
-import { useLayoutEffect } from 'react';
+import { Link, Stack } from 'expo-router';
 
 import { Icon } from '@/components/Icon';
 import PageContainer from '@/components/page-container';
 import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 
-const NAVIGATION_TITLE = '哎呀，页面不见啦！';
-
 export default function NotFoundScreen() {
-  // 设置导航栏标题
-  const navigation = useNavigation();
-  useLayoutEffect(() => {
-    navigation.setOptions({ title: NAVIGATION_TITLE });
-  }, [navigation]);
-
   return (
     <>
+      <Stack.Screen name="哎呀，页面不见啦！" />
+
       {/* 页面内容 */}
       <PageContainer className="items-center justify-center bg-background p-5">
         {/* 可爱表情图标 */}
@@ -24,12 +17,12 @@ export default function NotFoundScreen() {
 
         {/* 提示文字 */}
         <Text className="mt-6 text-center text-xl font-semibold">哎呀，这个页面被小猫叼走啦！</Text>
-        <Text className="mt-2 text-center text-base text-text-secondary">不用担心，你可以回到首页重新找路~</Text>
+        <Text className="text-text-secondary mt-2 text-center text-base">不用担心，你可以回到首页重新找路~</Text>
 
         {/* 返回首页按钮 */}
         <Link href="/" asChild replace>
           <Button className="mt-8" size="lg">
-            <Text className="text-lg text-white">回到首页</Text>
+            <Text className="text-lg">回到首页</Text>
           </Button>
         </Link>
       </PageContainer>
