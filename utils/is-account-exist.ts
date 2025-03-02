a86c1dcfd2ebc97f6c72ad87526ebe82e4821cff
ozline <ozlinex@outlook.com>
2025-03-03 02:16:17 +0800

    chore: fix outdated comment

--- utils/is-account-exist.ts
+++ utils/is-account-exist.ts
@@ -1,8 +1,8 @@
 import { LocalUser } from '@/lib/user';
 
 export async function isAccountExist() {
-  // 如果没有加载过用户信息，加载一次后再进行判断，这个 Load 和 _layout.tsx 中的 Load 都不能删
-  // 本地测试 iOS 和安卓的环境表现不一致，两个load都需要保留
+  // 如果没有加载过用户信息，加载一次后再进行判断
+  // 调用这个函数的入口在 @/app/(guest)/index.tsx，即开屏页，我们认为这是最早的入口
   if (!LocalUser.isLoaded) {
     await LocalUser.load();
   }
