6ce4062f2e638dc4a56bc19fa7deccd122c159d4
klxiaoniu <klxiaoniu@proton.me>
2025-03-05 00:29:49 +0800

    fix: crash
    Invariant Violation: scrollToIndex should be used in conjunction with getItemLayout or onScrollToIndexFailed, otherwise there is no way to know the location of offscreen indices or handle failures., js engine: hermes

--- components/tab-flatlist.tsx
+++ components/tab-flatlist.tsx
@@ -85,6 +85,7 @@ export function TabFlatList({
         onViewableItemsChanged={onViewableItemsChanged}
         viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
         renderItem={({ item }) => renderContent(item)}
+        onScrollToIndexFailed={() => {}}
       />
     </>
   );
