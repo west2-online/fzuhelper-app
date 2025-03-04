065802cdd5307f292ac1ffe5daa6cf3e45e49237
Baoshuo <i@baoshuo.ren>
2025-03-04 20:46:37 +0800

    chore: organize imports
    Co-authored-by: 155TuT <zhiyiwang155TuT@gmail.com>

--- app/(guest)/contributors.tsx
+++ app/(guest)/contributors.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import ContributorsDOMComponent from '@/components/dom/contributors';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 // 这个页面更多地作为一种测试用途，用于展示 DOM 组件的使用
 // 复杂的页面可以使用 DOM 组件来简化开发流程，毕竟相应的 Native 组件不太好做
