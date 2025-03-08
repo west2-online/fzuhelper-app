bd0d4f0951068c8a0084d8e1d015aa975d785260
ozlinex <ozlinex@outlook.com>
2025-03-09 03:10:47 +0800

    支持 iOS/iPadOS 下 Widget 功能 (#149)
    补齐了相关飞书文档：https://west2-online.feishu.cn/wiki/SitbwKuLriaL5bk7Wbicxdf7nYb
    Apple Watch 侧的 App 支持待正式版上线后再补齐

--- lib/course.ts
+++ lib/course.ts
@@ -7,13 +7,16 @@ import {
   COURSE_CURRENT_CACHE_KEY,
   COURSE_SETTINGS_KEY,
   COURSE_TERMS_LIST_KEY,
+  IOS_APP_GROUP,
 } from '@/lib/constants';
 import { MergedExamData } from '@/types/academic';
 import generateRandomColor, { clearColorMapping, getExamColor } from '@/utils/random-color';
+import { ExtensionStorage } from '@bacons/apple-targets';
 import * as ExpoWidgetsModule from '@bittingz/expo-widgets';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 import objectHash from 'object-hash';
+import { Platform } from 'react-native';
 import { getWeeksBySemester } from './locate-date';
 
 export type ParsedCourse = Omit<JwchCourseListResponse_Course, 'rawAdjust' | 'rawScheduleRules' | 'scheduleRules'> &
@@ -172,19 +175,39 @@ export class CourseCache {
         lastExamUpdateTime: this.lastExamUpdateTime,
       }),
     );
+
+    // 将数据保存到原生共享存储中，以便在小组件中调用
     const termsList = JSON.parse((await AsyncStorage.getItem(COURSE_TERMS_LIST_KEY)) ?? '[]');
     const term = (await readCourseSetting()).selectedSemester;
     const currentTerm = termsList.data.data.data.terms.find((termData: any) => termData.term === term);
     const maxWeek = getWeeksBySemester(currentTerm.start_date, currentTerm.end_date);
-    ExpoWidgetsModule.setWidgetData(
-      JSON.stringify({
-        courseData: this.cachedData,
-        examData: this.cachedExamData,
-        startDate: currentTerm.start_date,
-        maxWeek: maxWeek,
-      }),
-      Constants.expoConfig?.android?.package,
-    );
+    if (Platform.OS === 'ios') {
+      // 这里不能和安卓那样直接用 package，因为这个 identifier 可能会有多个
+      // 只能在常量中定义这个 identifier
+      const storage = new ExtensionStorage(IOS_APP_GROUP);
+      storage.set(
+        COURSE_CURRENT_CACHE_KEY,
+        JSON.stringify({
+          courseData: this.cachedData,
+          examData: this.cachedExamData,
+          lastCourseUpdateTime: this.lastCourseUpdateTime,
+          lastExamUpdateTime: this.lastExamUpdateTime,
+          startDate: currentTerm.start_date,
+          maxWeek: maxWeek,
+        }),
+      ); // 如果要改这个 KEY，需要同步修改 target 中原生代码
+      ExtensionStorage.reloadWidget(); // 保存后需要重载一次
+    } else if (Platform.OS === 'android') {
+      ExpoWidgetsModule.setWidgetData(
+        JSON.stringify({
+          courseData: this.cachedData,
+          examData: this.cachedExamData,
+          startDate: currentTerm.start_date,
+          maxWeek: maxWeek,
+        }),
+        Constants.expoConfig?.android?.package,
+      );
+    }
     console.log('Saved cached course data to widget.');
   }
 
@@ -201,7 +224,15 @@ export class CourseCache {
     this.priorityCounter = DEFAULT_PRIORITY; // 重置优先级计数器
     this.startID = DEFAULT_STARTID; // 重置 ID 计数器
     await AsyncStorage.removeItem(COURSE_CURRENT_CACHE_KEY);
-    ExpoWidgetsModule.setWidgetData('', Constants.expoConfig?.android?.package);
+
+    // 清除小组件数据
+    if (Platform.OS === 'ios') {
+      const storage = new ExtensionStorage(IOS_APP_GROUP);
+      storage.set(COURSE_CURRENT_CACHE_KEY, '');
+      ExtensionStorage.reloadWidget(); // 保存后需要重载一次
+    } else if (Platform.OS === 'android') {
+      ExpoWidgetsModule.setWidgetData('', Constants.expoConfig?.android?.package);
+    }
   }
 
   /**
