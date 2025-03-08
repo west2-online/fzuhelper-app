8ed8febdb618fe096be3e88614cc5a736289495f
ozline <ozlinex@outlook.com>
2025-03-09 02:34:44 +0800

    feat: delete Control widget and Live Activity

--- targets/iOS-widget/index.swift
+++ targets/iOS-widget/index.swift
@@ -4,9 +4,7 @@ import WidgetKit
 @main
 struct exportWidgets: WidgetBundle {
   var body: some Widget {
-    // 这里表示你会导出 3 个 Widget，如果有其他 Widget，在这里导出即可
+    // 这里表示你会导出需要的 Widget，如果有其他 Widget，在这里导出即可
     widget()  // 普通 Widget，标准的桌面组件
-    widgetControl()  // Control Widget，可以添加在右上下拉的控制中心中
-    //        WidgetLiveActivity() // iOS16 后的 Live Activity，暂时不启用，保留 Demo 代码
   }
 }
