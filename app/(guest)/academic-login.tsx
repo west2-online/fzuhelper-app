0cc46aa51c1954195b05f9ea6fdbb862a89cf487
klxiaoniu <klxiaoniu@proton.me>
2025-03-12 01:48:42 +0800

    fix: adapt pages

--- app/(guest)/academic-login.tsx
+++ app/(guest)/academic-login.tsx
@@ -11,6 +11,7 @@ import { Input } from '@/components/ui/input';
 import { Text } from '@/components/ui/text';
 
 import { getApiV1JwchUserInfo, getApiV1LoginAccessToken } from '@/api/generate';
+import PageContainer from '@/components/page-container';
 import { useRedirectWithoutHistory } from '@/hooks/useRedirectWithoutHistory';
 import { useSafeResponseSolve } from '@/hooks/useSafeResponseSolve';
 import aegis from '@/lib/aegis';
@@ -150,143 +151,145 @@ const LoginPage: React.FC = () => {
     <>
       <Stack.Screen options={{ title: '登录', headerShown: false }} />
 
-      <SafeAreaView className="bg-background">
-        <KeyboardAwareScrollView
-          className="h-full"
-          contentContainerStyle={styles.scrollViewContent}
-          keyboardShouldPersistTaps="handled"
-        >
-          <View className="flex-1 justify-between px-6 py-3">
-            {/* 左上角标题 */}
-            <View className="ml-1 mt-14">
-              <Text className="mb-2 text-4xl font-bold">{isPostGraduate ? '研究生' : '本科生'}登录</Text>
-              <Text className="text-lg text-text-secondary">综合性最强的福大校内APP</Text>
-            </View>
+      <PageContainer>
+        <SafeAreaView>
+          <KeyboardAwareScrollView
+            className="h-full"
+            contentContainerStyle={styles.scrollViewContent}
+            keyboardShouldPersistTaps="handled"
+          >
+            <View className="flex-1 justify-between px-6 py-3">
+              {/* 左上角标题 */}
+              <View className="ml-1 mt-14">
+                <Text className="mb-2 text-4xl font-bold">{isPostGraduate ? '研究生' : '本科生'}登录</Text>
+                <Text className="text-lg text-text-secondary">综合性最强的福大校内APP</Text>
+              </View>
 
-            {/* 页面内容 */}
-            <View className="items-center justify-center">
-              {/* 用户名输入框 */}
-              <Input
-                value={username}
-                onChangeText={setUsername}
-                placeholder="请输入学号"
-                className="my-4 w-full px-1 py-3"
-              />
+              {/* 页面内容 */}
+              <View className="items-center justify-center">
+                {/* 用户名输入框 */}
+                <Input
+                  value={username}
+                  onChangeText={setUsername}
+                  placeholder="请输入学号"
+                  className="my-4 w-full px-1 py-3"
+                />
 
-              {/* 密码输入框 */}
-              <Input
-                value={password}
-                onChangeText={setPassword}
-                placeholder="请输入密码"
-                secureTextEntry
-                className="mb-4 w-full px-1 py-3"
-              />
+                {/* 密码输入框 */}
+                <Input
+                  value={password}
+                  onChangeText={setPassword}
+                  placeholder="请输入密码"
+                  secureTextEntry
+                  className="mb-4 w-full px-1 py-3"
+                />
 
-              {/* 验证码输入框和图片 */}
-              {!isPostGraduate && (
-                <View className="mb-12 w-full flex-row items-center justify-between">
-                  <Input
-                    value={captcha}
-                    onChangeText={setCaptcha}
-                    placeholder="请输入验证码"
-                    className="mr-4 flex-1 px-1 py-3"
-                  />
-                  <TouchableOpacity onPress={refreshCaptcha}>
-                    {captchaImage ? (
-                      // 显示验证码图片
-                      <Image source={{ uri: captchaImage }} className="h-8 w-40" resizeMode="stretch" />
-                    ) : (
-                      // 显示灰色占位框
-                      <View className="h-8 w-40 items-center justify-center bg-gray-200">
-                        <Text className="text-xs text-gray-500">加载中...</Text>
-                      </View>
-                    )}
-                  </TouchableOpacity>
-                </View>
-              )}
+                {/* 验证码输入框和图片 */}
+                {!isPostGraduate && (
+                  <View className="mb-12 w-full flex-row items-center justify-between">
+                    <Input
+                      value={captcha}
+                      onChangeText={setCaptcha}
+                      placeholder="请输入验证码"
+                      className="mr-4 flex-1 px-1 py-3"
+                    />
+                    <TouchableOpacity onPress={refreshCaptcha}>
+                      {captchaImage ? (
+                        // 显示验证码图片
+                        <Image source={{ uri: captchaImage }} className="h-8 w-40" resizeMode="stretch" />
+                      ) : (
+                        // 显示灰色占位框
+                        <View className="h-8 w-40 items-center justify-center bg-gray-200">
+                          <Text className="text-xs text-gray-500">加载中...</Text>
+                        </View>
+                      )}
+                    </TouchableOpacity>
+                  </View>
+                )}
 
-              {/* 登录按钮 */}
-              <TouchableOpacity
-                onPress={isLoggingIn ? undefined : handleLogin}
-                disabled={isLoggingIn}
-                className={`mb-6 w-full items-center justify-center rounded-4xl py-3 ${
-                  isLoggingIn ? 'bg-gray-400' : 'bg-primary'
-                }`}
-              >
-                <Text className="text-lg font-bold text-white">{isLoggingIn ? '登录中...' : '登 录'}</Text>
-              </TouchableOpacity>
-
-              {/* 其他操作 */}
-              <View className="w-full flex-row justify-between px-2">
+                {/* 登录按钮 */}
                 <TouchableOpacity
-                  className="flex-row items-center"
-                  onPress={() => setIsPostGraduate(!isPostGraduate)}
-                  activeOpacity={0.5}
+                  onPress={isLoggingIn ? undefined : handleLogin}
+                  disabled={isLoggingIn}
+                  className={`mb-6 w-full items-center justify-center rounded-4xl py-3 ${
+                    isLoggingIn ? 'bg-gray-400' : 'bg-primary'
+                  }`}
                 >
-                  <Checkbox checked={isPostGraduate} onCheckedChange={setIsPostGraduate} />
-                  <Text className="text-center text-text-secondary">{'  '}我是研究生</Text>
+                  <Text className="text-lg font-bold text-white">{isLoggingIn ? '登录中...' : '登 录'}</Text>
                 </TouchableOpacity>
-                <Text className="text-primary" onPress={openResetPassword}>
-                  忘记密码
-                </Text>
+
+                {/* 其他操作 */}
+                <View className="w-full flex-row justify-between px-2">
+                  <TouchableOpacity
+                    className="flex-row items-center"
+                    onPress={() => setIsPostGraduate(!isPostGraduate)}
+                    activeOpacity={0.5}
+                  >
+                    <Checkbox checked={isPostGraduate} onCheckedChange={setIsPostGraduate} />
+                    <Text className="text-center text-text-secondary">{'  '}我是研究生</Text>
+                  </TouchableOpacity>
+                  <Text className="text-primary" onPress={openResetPassword}>
+                    忘记密码
+                  </Text>
+                </View>
+
+                {/* 公告栏 */}
+                <View className="mt-10 w-full px-1">
+                  <Text className="my-2 text-lg font-bold text-text-secondary">友情提示</Text>
+                  <Text className="text-base text-text-secondary">
+                    1. 教务处在夜间(23:00-06:00)例行维护，可能暂时无法登录
+                  </Text>
+                  <Text className="text-base text-text-secondary">
+                    2. 非校园网访问可能受阻。如果无法登录请尝试连接校园网
+                  </Text>
+                  {Platform.OS === 'ios' && (
+                    <Text className="text-base text-text-secondary">
+                      3. (仅iOS) 首次使用可能无法显示验证码，可以手动点击刷新
+                    </Text>
+                  )}
+                  {Platform.OS === 'ios' && (
+                    <Text className="text-base text-text-secondary">
+                      4. (仅iOS) 如果您意外拒绝了网络访问权限，您需要手动在设置中打开，APP 无法二次请求网络权限
+                    </Text>
+                  )}
+                </View>
               </View>
 
-              {/* 公告栏 */}
-              <View className="mt-10 w-full px-1">
-                <Text className="my-2 text-lg font-bold text-text-secondary">友情提示</Text>
-                <Text className="text-base text-text-secondary">
-                  1. 教务处在夜间(23:00-06:00)例行维护，可能暂时无法登录
-                </Text>
-                <Text className="text-base text-text-secondary">
-                  2. 非校园网访问可能受阻。如果无法登录请尝试连接校园网
-                </Text>
-                {Platform.OS === 'ios' && (
-                  <Text className="text-base text-text-secondary">
-                    3. (仅iOS) 首次使用可能无法显示验证码，可以手动点击刷新
+              {/* 底部协议 */}
+              <TouchableOpacity
+                activeOpacity={0.5}
+                className="mb-4 mt-12 w-full flex-row justify-center py-2"
+                onPress={() => setIsAgree(!isAgree)}
+              >
+                <Checkbox checked={isAgree} onCheckedChange={setIsAgree} />
+                <Text className="text-center text-text-secondary">
+                  {'  '}
+                  阅读并同意{' '}
+                  <Text
+                    className="text-primary"
+                    onPress={event => {
+                      event.stopPropagation();
+                      openUserAgreement();
+                    }}
+                  >
+                    服务协议
+                  </Text>{' '}
+                  和{' '}
+                  <Text
+                    className="text-primary"
+                    onPress={event => {
+                      event.stopPropagation();
+                      openPrivacyPolicy();
+                    }}
+                  >
+                    隐私政策
                   </Text>
-                )}
-                {Platform.OS === 'ios' && (
-                  <Text className="text-base text-text-secondary">
-                    4. (仅iOS) 如果您意外拒绝了网络访问权限，您需要手动在设置中打开，APP 无法二次请求网络权限
-                  </Text>
-                )}
-              </View>
+                </Text>
+              </TouchableOpacity>
             </View>
-
-            {/* 底部协议 */}
-            <TouchableOpacity
-              activeOpacity={0.5}
-              className="mb-4 mt-12 w-full flex-row justify-center py-2"
-              onPress={() => setIsAgree(!isAgree)}
-            >
-              <Checkbox checked={isAgree} onCheckedChange={setIsAgree} />
-              <Text className="text-center text-text-secondary">
-                {'  '}
-                阅读并同意{' '}
-                <Text
-                  className="text-primary"
-                  onPress={event => {
-                    event.stopPropagation();
-                    openUserAgreement();
-                  }}
-                >
-                  服务协议
-                </Text>{' '}
-                和{' '}
-                <Text
-                  className="text-primary"
-                  onPress={event => {
-                    event.stopPropagation();
-                    openPrivacyPolicy();
-                  }}
-                >
-                  隐私政策
-                </Text>
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </KeyboardAwareScrollView>
-      </SafeAreaView>
+          </KeyboardAwareScrollView>
+        </SafeAreaView>
+      </PageContainer>
     </>
   );
 };
--- components/page-container.tsx
+++ components/page-container.tsx
@@ -19,7 +19,7 @@ export function useSafeBottomTabBarHeight(): number {
   }
 }
 
-// 这个组件的作用是为页面提供一个背景色为 bg-background、高度至少撑满的容器
+// 这个组件的作用是为页面提供一个背景为 bg-background 颜色或自定义背景、高度至少撑满的容器
 // 如果需要自定义样式，可以通过 className 属性传入&覆盖
 // 页面编写时，应该将所有内容放在这个容器内。如果页面内容需要滚动，可以在这个容器内嵌套 ScrollView 组件
 export default function PageContainer({ className, ...otherProps }: ThemedViewProps) {
