0e43c6f1111ff83b51ed73d0f4d3ddabe76a0e6e
GoldenCreeperJ <jinzetan@foxmail.com>
2025-03-21 14:42:25 +0800

    listen free course

--- modules/native-widget/android/src/main/java/com/west2online/nativewidget/NativeWidgetModule.kt
+++ modules/native-widget/android/src/main/java/com/west2online/nativewidget/NativeWidgetModule.kt
@@ -1,14 +1,7 @@
 package com.west2online.nativewidget
 
-import android.util.Log
-import android.content.Context
-import android.content.SharedPreferences
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import android.content.Intent
-import android.content.ComponentName
-import android.appwidget.AppWidgetManager
-import android.content.pm.PackageManager
 
 class NativeWidgetModule : Module() {
     override fun definition() = ModuleDefinition {
