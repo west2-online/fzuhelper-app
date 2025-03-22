76090f2417f33d08a109be26c81416e5856c6fa5
Seeridia <Seeridia@gmail.com>
2025-03-22 19:00:49 +0800

    fix: 单人座等标识溢出不显示的错误

--- components/learning-center/seat-card.tsx
+++ components/learning-center/seat-card.tsx
@@ -9,18 +9,24 @@ interface SeatCardProps {
   isAvailable?: boolean;
 }
 
-const SeatCard: React.FC<SeatCardProps> = ({ spaceName, onPress, isAvailable = true }) => (
-  <View className="flex h-[56px] flex-1 p-1">
-    <TouchableOpacity
-      onPress={onPress}
-      className={cn(
-        'flex flex-1 items-center justify-center rounded-lg py-4',
-        isAvailable ? 'bg-green-200' : 'bg-red-200',
-      )}
-    >
-      <Text className={cn('text-center', isAvailable ? 'text-green-800' : 'text-red-800')}>{spaceName}</Text>
-    </TouchableOpacity>
-  </View>
-);
+const SeatCard: React.FC<SeatCardProps> = ({ spaceName, onPress, isAvailable = true }) => {
+  const hasMultipleLines = spaceName.includes('\n');
+
+  return (
+    <View className={cn('flex flex-1 p-1', hasMultipleLines ? 'h-[70px]' : 'h-[56px]')}>
+      <TouchableOpacity
+        onPress={onPress}
+        className={cn(
+          'flex flex-1 items-center justify-center rounded-lg py-2',
+          isAvailable ? 'bg-green-200' : 'bg-red-200',
+        )}
+      >
+        <Text className={cn('text-center', isAvailable ? 'text-green-800' : 'text-red-800')} numberOfLines={2}>
+          {spaceName}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default memo(SeatCard);
