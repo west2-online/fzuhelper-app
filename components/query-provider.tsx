754489cd66863080fa2b9e54b7e2b065abe6281b
Baoshuo <i@baoshuo.ren>
2025-04-02 22:30:03 +0800

    修正请求缓存逻辑

--- components/query-provider.tsx
+++ components/query-provider.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
