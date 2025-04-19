9ccdea098179f73fce0c79a0b42434a3096ca2bd
ayu 吴 <fakefish2569@outlook.com>
2025-04-19 20:01:25 +0800

    work5 052304113

--- types/data.d.ts
+++ types/data.d.ts
@@ -3,19 +3,4 @@ export interface JWCHLocateDateResult  {
   year: number; // 当前学年
   term: number; // 当前学期
 };
-export interface IdentifyRespData {
-  color: string; // 颜色
-  validTime: number; // 有效时间
-  content: string; // 内容
-}
-export interface PayCodeRespData {
-  devId: string;
-  expiredTime: string;
-  payAcctId: string;
-  payPrdCode: string;
-  prePayId: string;
-}
-interface LoginRespData {
-  name: string;
-  accessToken: string;
-}
+
