84a7649a88f5a91d2cefd90292764ef188ac9b22
linrongda <1945391294@qq.com>
2025-12-27 14:56:02 +0800

    test: apply new eas

--- app.config.ts
+++ app.config.ts
@@ -20,7 +20,7 @@ try {
 const versionCodePrefix = version.replace(/\./g, '');
 const versionCodeSuffix = String(commitCount).padStart(3, '0');
 // iOS
-const buildNumber = '721001';
+const buildNumber = '721002';
 // Android
 const versionCode = parseInt(buildNumber, 10);
 
