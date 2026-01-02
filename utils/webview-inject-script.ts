37569c966dfa40efb27ba359678bc992f11b9c07
klxiaoniu <klxiaoniu@proton.me>
2026-01-02 12:40:46 +0800

    修复iOS15下网页优化后表格被挤压等问题

--- utils/webview-inject-script.ts
+++ utils/webview-inject-script.ts
@@ -7,49 +7,59 @@ type GeneratedScript = (colorScheme: ParsedColorScheme) => string;
 type Script = StringScript | GeneratedScript;
 
 const commonScript: StringScript = `
-  // 设置 viewport
-  const metaViewport = document.createElement("meta");
-  metaViewport.name = "viewport";
-  metaViewport.content = "width=device-width,initial-scale=1.0";
-  document.head.appendChild(metaViewport);
+  (function() {
+    // 设置 viewport
+    const metaViewport = document.createElement("meta");
+    metaViewport.name = "viewport";
+    metaViewport.content = "width=device-width,initial-scale=1.0";
+    document.head.appendChild(metaViewport);
 
-  // 删除所有 table 的 width 属性
-  document.querySelectorAll("table").forEach((table) => {
-    table.removeAttribute("width");
-  });
+    // 删除所有 table 的 width 属性
+    document.querySelectorAll("table").forEach((table) => {
+      table.removeAttribute("width");
+      table.style.width = "100%";
+    });
 
-  // 删除所有 id 为 BT_print 的元素
-  document.querySelectorAll("#BT_print").forEach((element) => {
-    element.remove();
-  });
+    // 删除所有 id 为 BT_print 的元素
+    document.querySelectorAll("#BT_print").forEach((element) => {
+      element.remove();
+    });
 
-  document.querySelectorAll("td").forEach((td) => {
-    // 对所有 td 元素的内容进行 trim 操作
-    td.childNodes.forEach((node) => {
-      if (node.nodeType === Node.TEXT_NODE) {
-        node.textContent = node.textContent.trim();
+    document.querySelectorAll("td").forEach((td) => {
+      // 对所有 td 元素的内容进行 trim 操作
+      td.childNodes.forEach((node) => {
+        if (node.nodeType === Node.TEXT_NODE) {
+          node.textContent = node.textContent.trim();
+        }
+      });
+
+      // 设置 td 的 word-break 属性
+      td.style.wordBreak = "break-all";
+
+      // 修改 td 的 width 属性
+      const widthValue = td.getAttribute("width");
+      if (widthValue && /^[0-9]+$/.test(widthValue)) {
+        // 确保 width 是一个纯数字
+        const newWidthPercentage = (parseInt(widthValue, 10) / 800) * 100;
+        td.setAttribute("width", newWidthPercentage.toFixed(2) + "%");
       }
     });
 
-    // 设置 td 的 word-break 属性
-    td.style.wordBreak = "break-all";
-
-    // 修改 td 的 width 属性
-    const widthValue = td.getAttribute("width");
-    if (widthValue && /^[0-9]+$/.test(widthValue)) {
-      // 确保 width 是一个纯数字
-      const newWidthPercentage = (parseInt(widthValue, 10) / 800) * 100;
-      td.setAttribute("width", newWidthPercentage.toFixed(2) + "%");
-    }
-  });
+    // 滚动到顶部
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
+  })();
 `;
 
 const teachingProgramScript: StringScript = `
-  // 适用年级区域自动换行
-  const lbBtElement = document.getElementById("LB_bt");
-  if (lbBtElement) {
-    lbBtElement.style.wordBreak = "break-all";
-  }
+  (function() {
+    // 适用年级区域自动换行
+    const lbBtElement = document.getElementById("LB_bt");
+    if (lbBtElement) {
+      lbBtElement.style.wordBreak = "break-all";
+    }
+  })();
 `;
 
 type ColorData = {
@@ -75,40 +85,44 @@ const colors: Record<ParsedColorScheme, ColorData> = {
 };
 
 const darkModeScript: GeneratedScript = colorScheme => `
-  // 设置页面背景颜色
-  document.documentElement.style.colorScheme = "${colorScheme}";
-  document.body.style.backgroundColor = "${colors[colorScheme].backgroundColor}";
-  document.body.style.color = "${colors[colorScheme].color}";
+  (function() {
+    // 设置页面背景颜色
+    document.documentElement.style.colorScheme = "${colorScheme}";
+    document.body.style.backgroundColor = "${colors[colorScheme].backgroundColor}";
+    document.body.style.color = "${colors[colorScheme].color}";
 
-  // 删除所有 bgcolor 为 #FFFFFF 或 ffffff 的属性
-  document.querySelectorAll("[bgcolor='#FFFFFF'], [bgcolor='ffffff']").forEach((element) => {
-    element.removeAttribute("bgcolor");
-  });
+    // 删除所有 bgcolor 为 #FFFFFF 或 ffffff 的属性
+    document.querySelectorAll("[bgcolor='#FFFFFF'], [bgcolor='ffffff']").forEach((element) => {
+      element.removeAttribute("bgcolor");
+    });
 
-  document.querySelectorAll("table").forEach((table) => {
-    table.removeAttribute("bgcolor");
-    table.style.backgroundColor = "${colors[colorScheme].backgroundColor}";
-  });
+    document.querySelectorAll("table").forEach((table) => {
+      table.removeAttribute("bgcolor");
+      table.style.backgroundColor = "${colors[colorScheme].backgroundColor}";
+    });
 
-  document.querySelectorAll("tr").forEach((tr) => {
-    if (!tr.bgColor) {
-      // 设置表格行的背景颜色
-      tr.style.backgroundColor = "${colors[colorScheme].backgroundColor}";
-    } else if (/[\\w]{6}/.test(tr.bgColor)) {
-      // 表格高亮行的背景颜色
-      tr.style.backgroundColor = '#' + tr.bgColor + '${colors[colorScheme].backgroundColorAlpha}';
-      tr.removeAttribute("bgcolor");
-    } else {
-      tr.style.color = 'black';
-    }
-  });
+    document.querySelectorAll("tr").forEach((tr) => {
+      if (!tr.bgColor) {
+        // 设置表格行的背景颜色
+        tr.style.backgroundColor = "${colors[colorScheme].backgroundColor}";
+      } else if (/[\\w]{6}/.test(tr.bgColor)) {
+        // 表格高亮行的背景颜色
+        tr.style.backgroundColor = '#' + tr.bgColor + '${colors[colorScheme].backgroundColorAlpha}';
+        tr.removeAttribute("bgcolor");
+      } else {
+        tr.style.color = 'black';
+      }
+    });
+  })();
 `;
 
 const pyjhDarkModeScript: GeneratedScript = colorScheme => `
-  document.querySelectorAll("[bgcolor='efefef']").forEach((element) => {
-    element.removeAttribute("bgcolor");
-    element.style.backgroundColor = "${colors[colorScheme].backgroundHighlightColor}";
-  });
+  (function() {
+    document.querySelectorAll("[bgcolor='efefef']").forEach((element) => {
+      element.removeAttribute("bgcolor");
+      element.style.backgroundColor = "${colors[colorScheme].backgroundHighlightColor}";
+    });
+  })();
 `;
 
 // url 与脚本常量对应 map
