f168ff48baeb0437cd83ed20431c02e51760fca4
Lin Rongda <102654433+linrongda@users.noreply.github.com>
2026-03-02 17:55:07 +0800

    fix: #350 问题修复 (#352)

--- app/settings/calendar.tsx
+++ app/settings/calendar.tsx
@@ -19,7 +19,7 @@ import { fetchWithCache } from '@/utils/fetch-with-cache';
 
 export default function PersonalInfoListPage() {
   const [settings, setSettings] = useState<CourseSetting>(defaultCourseSetting);
-  const [switchLoading, setSwitchLoading] = useState(false);
+  const [switchDisabled, setSwitchDisabled] = useState(false);
 
   const readSettingsFromStorage = useCallback(async () => {
     console.log('读取课程设置');
@@ -32,7 +32,7 @@ export default function PersonalInfoListPage() {
   }, [readSettingsFromStorage]);
 
   const handleSubscribeChange = useCallback(async () => {
-    setSwitchLoading(true);
+    setSwitchDisabled(true);
     try {
       if (!settings.calendarExportEnabled) {
         // 如果用户开启了日历订阅，调用接口获取订阅地址，本地缓存 30 天。
@@ -69,7 +69,7 @@ export default function PersonalInfoListPage() {
     } catch (error) {
       console.error('订阅地址获取失败:', error);
     } finally {
-      setSwitchLoading(false);
+      setSwitchDisabled(false);
     }
   }, [settings.calendarExportEnabled]);
 
@@ -97,7 +97,7 @@ export default function PersonalInfoListPage() {
               label="启用日历订阅"
               value={settings.calendarExportEnabled}
               onValueChange={handleSubscribeChange}
-              loading={switchLoading}
+              disabled={switchDisabled}
             />
             {settings.calendarExportEnabled && (
               <View>
