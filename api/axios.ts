c2241b93701e6233d147c57c3df0c60ef96f1d52
klxiaoniu <klxiaoniu@proton.me>
2026-03-02 20:58:41 +0800

    refactor: extract common headers for axios requests
    - Moved common headers into a separate variable for better maintainability.
    - Updated the request interceptor to use the new common headers.

--- api/axios.ts
+++ api/axios.ts
@@ -11,19 +11,17 @@ import DeviceInfo from 'react-native-device-info';
 
 const baseURL = 'https://fzuhelper.west2.online/';
 
-const request = axios.create({
-  baseURL,
-  timeout: 5000,
-  headers: {
-    // 禁用本地缓存
-    'Cache-Control': 'no-cache, no-store, must-revalidate',
-    Pragma: 'no-cache',
-    Expires: '0',
-    // 私有接口请求头
-    'X-App-Version': DeviceInfo.getBuildNumber(),
-    'X-App-Platform': Platform.OS,
-  },
-});
+const commonHeaders = {
+  // 禁用本地缓存
+  'Cache-Control': 'no-cache, no-store, must-revalidate',
+  Pragma: 'no-cache',
+  Expires: '0',
+  // 私有接口请求头
+  'X-App-Version': DeviceInfo.getBuildNumber(),
+  'X-App-Platform': Platform.OS,
+};
+
+const request = axios.create({ baseURL, timeout: 5000, headers: commonHeaders });
 
 type PromiseExecutorParameters = Parameters<ConstructorParameters<typeof Promise<AxiosResponse<any, any>>>[0]>;
 // 请求队列
@@ -93,6 +91,7 @@ request.interceptors.response.use(
             timeout: 5000,
             headers: {
               Authorization: await AsyncStorage.getItem(REFRESH_TOKEN_KEY),
+              ...commonHeaders,
             },
           });
           if (res.data.code !== ResultEnum.SuccessCode) {
