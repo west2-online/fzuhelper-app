3b0b22a4159d5a676807fec9697c14e8f9d507a9
klxiaoniu <klxiaoniu@proton.me>
2026-03-02 14:43:31 +0800

    添加iOS部分代码

--- modules/native-widget/ios/NativeWidgetModule.swift
+++ modules/native-widget/ios/NativeWidgetModule.swift
@@ -3,5 +3,10 @@ import ExpoModulesCore
 public class NativeWidgetModule: Module {
   public func definition() -> ModuleDefinition {
     Name("NativeWidget")
+
+    AsyncFunction("requestPinAppWidget") { (requestCode:Int) -> Int in
+      // iOS只能手动添加
+      return 2
+    }
   }
 }
