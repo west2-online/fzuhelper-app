7dd9bbf93c095ccb85f62420844ce5de2bdf2262
Cai <13110818005@qq.com>
2026-04-23 14:23:46 +0800

    feat(android): 新增一码通控制中心入口，修复iOS控制中心跳转 (#404)
    * feat(android): 新增一码通控制中心入口
    
    * style: 文件末尾补充空行
    
    * chore: 简化标签，与APP保持一致
    
    * chore: 改用`qr_action`图标
    
    * fix: iOS改用scheme跳转
    
    * refactor: Android改用scheme跳转
    
    ---------
    
    Co-authored-by: klxiaoniu <klxiaoniu@proton.me>

--- targets/iOS-widget/_shared/ControlWidget.swift
+++ targets/iOS-widget/_shared/ControlWidget.swift
@@ -5,8 +5,6 @@
 //  Created by 黄骁 on 2025/3/22.
 //
 
-// MARK: 这个文件涉及到 Universal Link，需要放在 _shared 文件夹下，否则无法自动关联 Target Membership，导致 Universal Link 无法打开（显示没有权限）
-
 import AppIntents
 import SwiftUI
 import WidgetKit
@@ -33,11 +31,10 @@ struct OpenAppWithURLIntent: AppIntent {
     static var openAppWhenRun = true
     static var isDiscoverable = true
 
+    @MainActor
     func perform() async throws -> some IntentResult & OpensIntent {
-        // 使用 Universal Link
-        // MARK: 这个 Universal Link 需要 fzuhelper-web，也就是我们官网的 AASA 文件配合，以及 app 内的配合，这个 /qrcode 路径不要修改，直接对应 App 内的 Expo-Router 设置的 /qrcode 页面，即一码通
-//        let url = URL(string: "https://fzuhelperapp.west2.online/(guest)/?method=qrcode")! // 这个设计保留，但暂时不使用
-        let url = URL(string: "https://fzuhelperapp.west2.online/qrcode")!
-        return .result(opensIntent: OpenURLIntent(url))
+        let url = URL(string: "fzuhelper://qrcode")!
+        EnvironmentValues().openURL(url)
+        return .result()
     }
 }
