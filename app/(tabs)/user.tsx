4c3bd6ab64d020dc0711f27df3d586c66fcd44bc
155TuT <zhiyiwang155TuT@gmail.com>
2026-04-26 23:35:17 +0800

    feat: 替换好友管理入口为专属图标
    在user页面中替换占位图标为好友管理专属图标，移除临时注释

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -15,6 +15,7 @@ import AvatarDefault from '@/assets/images/my/avatar_default.png';
 import CalendarIcon from '@/assets/images/my/ic_calendar.png';
 import EcardIcon from '@/assets/images/my/ic_ecard.png';
 import HelpIcon from '@/assets/images/my/ic_help.png';
+import FriendManageIcon from '@/assets/images/my/ic_friend_manage.png';
 import useApiRequest from '@/hooks/useApiRequest';
 
 const defaultUserInfo: UserInfo = {
@@ -51,9 +52,8 @@ export default function UserPage() {
       name: '校历',
       link: '/common/academic-calendar' as Href,
     },
-    //图标等美术画好了再换
     {
-      icon: CalendarIcon,
+      icon: FriendManageIcon,
       name: '好友管理',
       link: '/settings/friend/list' as Href,
     },
