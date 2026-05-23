27d40dabbe36f8bb6595893c618afe856d69e70c
GoldenCreeperJ <jinzetan@foxmail.com>
2026-05-23 23:41:57 +0800

    fix:full qrCode img saved

--- plugins/with-keep-resources.js
+++ plugins/with-keep-resources.js
@@ -1,7 +1,21 @@
 const { withAndroidManifest } = require('@expo/config-plugins');
+const ICON_FAMILIES = [
+  'qrcode',
+  'qrcode_foreground',
+  'qrcode_round'
+];
+
 module.exports = (config) => withAndroidManifest(config, (mod) => {
   const app = mod.modResults.manifest.application[0];
   app['meta-data'] = app['meta-data'] || [];
-  app['meta-data'].push({ $: { 'android:name': '__keep_qrcode', 'android:resource': '@mipmap/qrcode' } });
+  ICON_FAMILIES.forEach(name => {
+    app['meta-data'].push({
+      $: {
+        // 这里的 name 必须唯一，避免冲突
+        'android:name': `__keep_${name}_for_shrinker`,
+        'android:resource': `@mipmap/${name}`
+      }
+    });
+  });
   return mod;
 });
