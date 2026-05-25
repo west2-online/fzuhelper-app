dc9f290ab74733454850be125f2990a966700e13
GoldenCreeperJ <jinzetan@foxmail.com>
2026-05-25 10:45:19 +0800

    - 升级 `expo-quick-actions` 从 4.0.2 到 6.0.1 - 在 `with-keep-resources.js` 中添加说明

--- plugins/with-keep-resources.js
+++ plugins/with-keep-resources.js
@@ -1,3 +1,7 @@
+// expo-quick-actions 只在 Manifest 中保留了 shortcuts.xml, 而没有保留 Icon
+// 在 R8 激进的资源压缩策略下将会裁剪掉资源文件，导致二维码快捷方式的图标消失
+// 这里显式地将二维码图标写入 Manifest, 确保资源文件不被裁剪
+
 const { withAndroidManifest } = require('@expo/config-plugins');
 const ICON_FAMILIES = [
   'qrcode',
