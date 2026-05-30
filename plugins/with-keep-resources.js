3e7054aac2d6caf39f4f595386be1fa254ef54ea
GoldenCreeperJ <jinzetan@foxmail.com>
2026-05-30 13:09:55 +0800

    - 升级 `expo-quick-actions` 从 6.0.1 到 6.0.2 - 在 `with-keep-resources.js` 中添加说明

--- plugins/with-keep-resources.js
+++ plugins/with-keep-resources.js
@@ -1,4 +1,4 @@
-// expo-quick-actions 只在 Manifest 中保留了 shortcuts.xml, 而没有保留 Icon
+// expo-quick-actions 没有以任何方式显式声明调用或保留资源文件
 // 在 R8 激进的资源压缩策略下将会裁剪掉资源文件，导致二维码快捷方式的图标消失
 // 这里显式地将二维码图标写入 Manifest, 确保资源文件不被裁剪
 
