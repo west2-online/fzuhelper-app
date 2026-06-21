2002b53a65da1b398df22d9d3054fb38cd807785
klxiaoniu <klxiaoniu@proton.me>
2026-06-21 17:52:58 +0800

    修复iOS可能的异常

--- modules/safe-area-webview/src/SafeAreaWebView.tsx
+++ modules/safe-area-webview/src/SafeAreaWebView.tsx
@@ -2,16 +2,16 @@ import { forwardRef } from 'react';
 import { Platform, requireNativeComponent, type HostComponent } from 'react-native';
 import { WebView, type WebViewProps } from 'react-native-webview';
 
-const FzuSafeAreaWebView = requireNativeComponent('FzuSafeAreaWebView') as HostComponent<any>;
-
 /**
  * 定制Webview，不向网页传入safe-area-inset-*等环境变量，因为我们布局已经保证不会渲染到安全区
- * 目前Android only
+ * Android only, iOS 默认不会传入
  */
 export const SafeAreaWebView = forwardRef<WebView, WebViewProps>((props, ref) => {
   if (Platform.OS !== 'android') {
     return <WebView {...props} ref={ref} />;
   }
+
+  const FzuSafeAreaWebView = requireNativeComponent('FzuSafeAreaWebView') as HostComponent<any>;
 
   return <WebView {...props} ref={ref} nativeConfig={{ ...props.nativeConfig, component: FzuSafeAreaWebView }} />;
 });
