909ec0461dfd1ff7dc714175b173477b75187315
GoldenCreeperJ <jinzetan@foxmail.com>
2026-06-21 18:15:27 +0800

    refactor(android): 修正 Android 权限 maxSdk 版本配置插件

--- plugins/with-android-maxSdkVersion.js
+++ plugins/with-android-maxSdkVersion.js
@@ -1,18 +1,26 @@
 const { withAndroidManifest } = require('@expo/config-plugins');
 
-module.exports = function withMaxSdkVersion(config, props) {
+module.exports = function withPermissionMaxSdkVersion(config) {
   return withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults.manifest;
 
-    if (!androidManifest['uses-sdk']) {
-      androidManifest['uses-sdk'] = [{ $: {} }];
-    } else if (!androidManifest['uses-sdk'][0]['$']) {
-      androidManifest['uses-sdk'][0]['$'] = {};
+    if (!androidManifest['uses-permission']) {
+      androidManifest['uses-permission'] = [];
     }
 
-    const sdkAttrs = androidManifest['uses-sdk'][0]['$'];
-    sdkAttrs['android:maxSdkVersion'] = props.maxSdkVersion.toString();
-    sdkAttrs['tools:replace'] = 'android:maxSdkVersion';
+    const targetPermissions = [
+      'android.permission.READ_EXTERNAL_STORAGE',
+      'android.permission.WRITE_EXTERNAL_STORAGE',
+    ];
+
+    androidManifest['uses-permission'].forEach((perm) => {
+      const attrs = perm.$ || {};
+      const name = attrs['android:name'];
+      if (targetPermissions.includes(name)) {
+        attrs['android:maxSdkVersion'] = '29';
+        attrs['tools:replace'] = 'android:maxSdkVersion';
+      }
+    });
 
     return config;
   });
