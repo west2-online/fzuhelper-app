c0d1876f59de7559bcb8ecb4e4815af7e9c340b7
wild-gooseee <3472434074@qq.com>
2026-08-02 18:59:02 +0800

    feat: 更新工具箱，用综合预约替代旧学习中心

--- app/(tabs)/toolbox.tsx
+++ app/(tabs)/toolbox.tsx
@@ -177,13 +177,6 @@ const DEFAULT_TOOLS: ToolboxTool[] = [
   },
   {
     id: 150,
-    name: '学习中心',
-    icon: StudyCenterIcon,
-    type: ToolType.LINK,
-    href: '/toolbox/learning-center',
-  },
-  {
-    id: 151,
     name: '综合预约',
     icon: StudyCenterIcon,
     type: ToolType.WEBVIEW,
