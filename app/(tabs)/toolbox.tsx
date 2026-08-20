00194540307a81c71040e21c62d8ee36f2b54386
Baoshuo <i@baoshuo.ren>
2026-08-20 23:24:52 +0800

    Apply suggestions from code review
    Co-authored-by: Baoshuo <i@baoshuo.ren>

--- app/(tabs)/toolbox.tsx
+++ app/(tabs)/toolbox.tsx
@@ -315,7 +315,7 @@ const DEFAULT_TOOLS: DefaultToolboxTool[] = [
   },
   {
     id: 200,
-    name: '水电缴费测试',
+    name: '水电缴费',
     icon: UtilityPaymentIcon,
     type: ToolType.LINK,
     href: '/toolbox/utility-payment',
@@ -329,14 +329,14 @@ const DEFAULT_TOOLS: DefaultToolboxTool[] = [
   },
   {
     id: 220,
-    name: '招聘会测试',
+    name: '招聘会',
     icon: JobFiarIcon,
     type: ToolType.LINK,
     href: '/toolbox/job-fair',
   },
   {
     id: 230,
-    name: '晚点名签到测试',
+    name: '晚点名签到',
     icon: SignIcon,
     type: ToolType.WEBVIEW,
     params: {
